3c925e968e2a58aaa7fdcd8d8827cd1f14876fe6
march <march.creative27@gmail.com>
2025-07-31 13:30:53 +0700

    contacts api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContactService } from './contact/contact.service';
+import { ContactController } from './contact/contact.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ContactController],
+  providers: [AppService, ContactService],
 })
 export class AppModule {}
